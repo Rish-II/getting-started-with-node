0fa7400c6c0675620528043df45ec6612afe0ec1
Rish-II <99901203+Rish-II@users.noreply.github.com>
2022-02-22 23:18:58 +0530

    Update server.js

--- server.js
+++ server.js
@@ -11,5 +11,19 @@ app.get('/', (req, res) => {
   res.send('Hello World - NodeJS is running on FlashDrive!');
 });
 
+var WebSocketServer = require('ws').Server,
+  wss = new WebSocketServer({port: 40510})
+
+wss.on('connection', function (ws) {
+  ws.on('message', function (message) {
+    console.log('received: %s', message)
+  })
+
+  setInterval(
+    () => ws.send(`${new Date()}`),
+    1000
+  )
+});
+
 app.listen(PORT, HOST);
 console.log(`Running on http://${HOST}:${PORT}`);
